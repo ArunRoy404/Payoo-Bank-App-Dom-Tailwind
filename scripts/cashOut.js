d6c9186d1215dcea23024de52d1a596dcb62f34b
ArunRoy404 <arunroy0065@gmail.com>
2025-02-27 13:00:26 +0600

    cashout function added

---
+++ scripts/cashOut.js
@@ -0,0 +1,20 @@
+document.getElementById('cash-out-btn').addEventListener('click',function(event){
+    event.preventDefault()
+
+    let balance = getNumber('balance')
+    const accountNumber = getInputNumberByID('cash-out-account')
+    const amount = getInputNumberByID('cash-out-amount')
+    const pin = getInputNumberByID('cash-out-pin')
+
+    if(balance<amount){
+        alert('Invalid Amount')
+    }else{
+        if(pin === 1234){
+            balance -= amount
+            console.log(balance)
+            setText('balance',balance)
+        }else{
+            alert('Wrong Credential')
+        }
+    }
+})
